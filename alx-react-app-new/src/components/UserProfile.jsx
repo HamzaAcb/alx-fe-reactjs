8700c7fdb9e27438153fa43a357281b61c47b547
HamzaAcb <hamzaachoubir98@gmail.com>
2024-08-15 08:43:50 -0400

    UserProfile comp styled

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 const UserProfile = (props) => {
     return (
-      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' , backgroundColor: 'grey'}}>
-        <h2 style={{ color: 'purple' }}>{props.name}</h2>
-        <p style={{ fontWeight: 'bold' }}>Age: <span>{props.age}</span></p>
-        <p style={{ color: 'yellow' }}>Bio: {props.bio}</p>
+      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+        <h2 style={{ color: 'blue' }}>{props.name}</h2>
+        <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+        <p>Bio: {props.bio}</p>
       </div>
     );
   };
