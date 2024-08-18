9bcd5cb5de6c3756090894090c1233abb993b023
HamzaAcb <hamzaachoubir98@gmail.com>
2024-08-18 12:56:48 -0400

    Build a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', backgroundColor: '#f0f0f0', borderTop: '1px solid #ccc', textAlign: 'center' }}>
+        <p>&copy; 2024 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
