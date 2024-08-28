dba510ce434cb8136bad9ea5528d9bd61b641a9f
HamzaAcb <hamzaachoubir98@gmail.com>
2024-08-28 05:32:13 -0400

    extanding the store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,6 +4,8 @@ const useRecipeStore = create(set => ({
   recipes: [],
   searchTerm: '',
   filteredRecipes: [],
+  
+  // Actions for managing recipes
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   updateRecipe: (updatedRecipe) => set(state => ({
     recipes: state.recipes.map(recipe =>
@@ -13,12 +15,18 @@ const useRecipeStore = create(set => ({
   deleteRecipe: (id) => set(state => ({
     recipes: state.recipes.filter(recipe => recipe.id !== id)
   })),
+  
+  // Actions for search and filter
   setSearchTerm: (term) => set({ searchTerm: term }),
   filterRecipes: () => set(state => ({
     filteredRecipes: state.recipes.filter(recipe =>
       recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
     )
-  }))
+  })),
+  
+  // New action for setting the recipes directly
+  setRecipes: (recipes) => set({ recipes })
 }));
 
 export default useRecipeStore;
+
