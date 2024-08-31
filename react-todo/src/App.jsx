7cfd2d2f085cb2076240fdd59e87fdf2c5fcb145
HamzaAcb <hamzaachoubir98@gmail.com>
2024-08-31 12:00:10 -0400

    Fixing the Test problems

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import TodoList from './components/TodoList'; // Adjust the path if necessary
+import TodoList from './components/TodoList';
 
 const App = () => {
   return (
     <div>
-      <h1>My Todo List App</h1>
+      <h1>My Todo App</h1>
       <TodoList />
     </div>
   );
