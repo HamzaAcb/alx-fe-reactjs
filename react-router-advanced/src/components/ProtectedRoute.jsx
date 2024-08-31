6526c83f43270528f0260166238b6e0fec6fcb33
HamzaAcb <hamzaachoubir98@gmail.com>
2024-08-31 11:09:10 -0400

    Modifying the component

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 function ProtectedRoute({ isAuthenticated }) {
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+  // If the user is not authenticated, redirect to the home page or a login page
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  // If the user is authenticated, render the requested route
+  return <Outlet />;
 }
 
 export default ProtectedRoute;
+
