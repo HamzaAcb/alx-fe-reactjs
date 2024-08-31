d77ef84ce48c4245822b0e44ffb3abc9c41351a5
HamzaAcb <hamzaachoubir98@gmail.com>
2024-08-31 07:36:44 -0400

    adding missing properties

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,19 +1,37 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
+// Function to fetch posts
 function fetchPosts() {
   return axios.get('https://jsonplaceholder.typicode.com/posts');
 }
 
 function PostsComponent() {
-  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+  const {
+    data,
+    error,
+    isLoading,
+    isError,
+    refetch,
+    isFetching
+  } = useQuery(
+    'posts', 
+    fetchPosts, 
+    {
+      // Configuration options
+      cacheTime: 5 * 60 * 1000, // 5 minutes
+      staleTime: 1 * 60 * 1000,  // 1 minute
+      refetchOnWindowFocus: true, // Refetch data when window gains focus
+      keepPreviousData: true,     // Keep previous data while new data is loading
+    }
+  );
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>An error occurred: {error.message}</div>;
 
   return (
     <div>
-      <button onClick={() => refetch()}>Refetch Data</button>
+      <button onClick={() => refetch()} disabled={isFetching}>Refetch Data</button>
       <ul>
         {data.data.map(post => (
           <li key={post.id}>{post.title}</li>
@@ -24,4 +42,5 @@ function PostsComponent() {
 }
 
 export default PostsComponent;
+
 
